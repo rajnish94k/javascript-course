4b5eef300a58cc8a562b66c0bd8772689d1f2f0b
rajnish <rajnish94k@gmail.com>
2021-12-17 18:30:49 +0530

    array completed

--- Fundamental 2/assets/scripts/app.js
+++ Fundamental 2/assets/scripts/app.js
@@ -129,15 +129,37 @@
 // console.log(ages);
 
 // ==================== Basic array operation methods ===================
-//  const friends = ['Michael', 'Steven', 'Peter'];
-//  friends.push('Jay');
-// Add elements
-// const newLength = friends.push('Jay');
-//  console.log(friends);
-//  console.log(newLength);
+ const friends = ['Michael', 'Steven', 'Peter'];
+ friends.push('Jay');
 
-//  friends.unshift('John');
-//  console.log(friends);
+// ============== Add elements
+const newLength = friends.push('Jay');
+ console.log(friends);
+ console.log(newLength);
+
+ friends.unshift('John');
+ console.log(friends);
+
+// ============== Remove elements =====
+friends.pop(); // Last
+const poped = friends.pop();
+console.log(poped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend Steven');
+}
 
  // ==================== Objects ===================
 
